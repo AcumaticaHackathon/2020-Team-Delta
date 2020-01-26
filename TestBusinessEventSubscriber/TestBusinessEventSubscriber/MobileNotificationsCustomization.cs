114eb42d338f34cfc00f14a6b3c6a7f4738af26a
Dmitrii-Naumov <46959488+Dmitrii-Naumov@users.noreply.github.com>
2020-01-26 13:46:07 -0500

    Add First Visual Studio Project with Subscriber

---
+++ TestBusinessEventSubscriber/TestBusinessEventSubscriber/MobileNotificationsCustomization.cs
@@ -0,0 +1,68 @@
+﻿using MSTeamsIntegration;
+using PX.BusinessProcess.DAC;
+using PX.BusinessProcess.UI;
+using PX.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBusinessEventSubscriber
+{
+	public class MobileNotificationExt : PXCacheExtension<MobileNotification>
+	{
+		#region NTo
+
+		/// <exclude/>
+		public abstract class usrTo : PX.Data.BQL.BqlString.Field<usrTo> { }
+
+		[PXSelector(typeof(Search<TeamsPlugin.name>))]
+		[PXUIField(DisplayName = "To")]
+		[PXDBString(40)]
+		public virtual string UsrTo { get; set; }
+
+		#endregion
+	}
+	public class MobileNotificationsCustomization : PXGraphExtension<MobileNotificationMaint>
+	{
+		[PXMergeAttributes(Method = MergeMethod.Merge)]
+
+		[PXIntList(new int[] { 1, 2, 3 }, new string[] { "Push", "SMS", "Teams" })]
+		public void MobileNotification_DeliveryType_CacheAttached(PXCache sender)
+		{
+		}
+		protected virtual void MobileNotification_RowSelected(PXCache cache, PXRowSelectedEventArgs e, PXRowSelected baseMethod)
+		{
+			if (baseMethod != null)
+				baseMethod.Invoke(cache, e);
+			var row = e.Row as MobileNotification;
+			if (row == null)
+				return;
+			bool isTeamsNotification = row.DeliveryType == (byte)3;
+			PXUIFieldAttribute.SetVisible<MobileNotification.destinationEntityID>(cache, row, !isTeamsNotification);
+			PXUIFieldAttribute.SetVisible<MobileNotification.destinationScreenID>(cache, row, !isTeamsNotification);
+			PXUIFieldAttribute.SetVisible<MobileNotification.subject>(cache, row, !isTeamsNotification);
+			PXUIFieldAttribute.SetVisible<MobileNotification.nfrom>(cache, row, !isTeamsNotification);
+			PXUIFieldAttribute.SetVisible<MobileNotification.nto>(cache, row, !isTeamsNotification);
+			PXUIFieldAttribute.SetVisible<MobileNotificationExt.usrTo>(cache, row, isTeamsNotification);
+
+			PXDefaultAttribute.SetPersistingCheck<MobileNotification.nto>(cache, row, isTeamsNotification ? PXPersistingCheck.Nothing : PXPersistingCheck.NullOrBlank);
+
+		}
+		protected virtual void MobileNotification_RowPersisting(PXCache cache, PXRowPersistingEventArgs e, PXRowPersisting baseMethod)
+		{
+			var row = e.Row as MobileNotification;
+			if (row == null)
+				return;
+			bool isTeamsNotification = row.DeliveryType == (byte)3;
+			if (isTeamsNotification)
+			{
+				PXDefaultAttribute.SetPersistingCheck<MobileNotification.nto>(cache, row,  PXPersistingCheck.Nothing );
+				row.NTo = (string)cache.GetValue(row, "UsrTo");
+
+			}
+			else baseMethod?.Invoke(cache, e);
+		}
+	}
+}
---
+++ TestBusinessEventSubscriber/TestBusinessEventSubscriber/Subscriber.cs
@@ -0,0 +1,89 @@
+﻿using MSTeamsIntegration;
+using PX.Api.Mobile.PushNotifications;
+using PX.Api.SMS;
+using PX.BusinessProcess.DAC;
+using PX.BusinessProcess.Event;
+using PX.BusinessProcess.Subscribers;
+using PX.BusinessProcess.Subscribers.ActionHandlers;
+using PX.BusinessProcess.Subscribers.Factories;
+using PX.BusinessProcess.UI;
+using PX.Data;
+using PX.Data.PushNotifications;
+using PX.SmsProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestBusinessEventSubscriber
+{
+	public class TestSubscriber : IBPSubscriberActionHandlerFactory
+	{
+		private readonly ISmsSender _sender;
+		private readonly IPushNotificationDefinitionProvider _pushDefinitionsProvider;
+		private IReadOnlyDictionary<string, ISmsProviderFactory> _providerFactories;
+		public string Type { get { return "TBED"; } }
+		public string TypeName { get { return "Team Delta Teams Message"; } }
+		public string CreateActionName
+		{
+			get { return "TeamsNotification"; }
+		}
+		public string CreateActionLabel
+		{
+			get { return "Team Delta Teams Message"; }
+		}
+		public TestSubscriber(ISmsSender sender, IPushNotificationDefinitionProvider pushDefinitionsProvider, IReadOnlyDictionary<string, ISmsProviderFactory> providerFactories)
+		{
+			_sender = sender;
+			_pushDefinitionsProvider = pushDefinitionsProvider;
+			_providerFactories = providerFactories;
+		}
+		public IEventAction CreateActionHandler(Guid handlerId, bool stopOnError, EventDefinitionsBySource eventDefinitionsProvider)
+		{
+			var graph = PXGraph.CreateInstance<PXGraph>();
+			var template =
+				PXSelect<MobileNotification,
+						Where<MobileNotification.noteID, Equal<Required<MobileNotification.noteID>>>>
+					.Select(graph, handlerId).SingleOrDefault();
+			return template != null ? new TeamsSendMessageAction(handlerId) : (IEventAction)new DummyAction(Type, handlerId);
+		}
+
+		/// <summary>
+		/// Returns BPHandler referencing particular message template. 
+		/// Is used for Subscriber ID selector
+		/// </summary>
+		/// <param name="graph"></param>
+		/// <returns></returns>
+		public IEnumerable<BPHandler> GetHandlers(PXGraph graph)
+		{
+			graph.Clear(PXClearOption.ClearQueriesOnly);
+			var result = PXSelect<MobileNotification,
+				   Where<MobileNotification.deliveryType, Equal<Required<MobileNotification.deliveryType>>
+				  >>.Select(graph, 3).FirstTableItems;
+
+			var resultReadOnly = PXSelectReadonly<MobileNotification,
+			   Where<MobileNotification.deliveryType, Equal<Required<MobileNotification.deliveryType>>
+			  >>.Select(graph, 3).FirstTableItems;
+
+			var result2 =PXSelect<MobileNotification,
+				   Where<MobileNotification.deliveryType, Equal<Required<MobileNotification.deliveryType>>,
+				   And<Where<MobileNotification.screenID, Equal<Current<BPEvent.screenID>>,
+						  Or<Current<BPEvent.screenID>, IsNull>>>>>
+					  .Select(graph, 3).FirstTableItems.Where(c => c != null).Select(c =>
+						  new BPHandler() { Id = c.NoteID, Name = c.Name, Type = TypeName });
+			return result2;
+		}
+		/// <summary>
+		/// Redirects to Graph with notification configuration
+		/// </summary>
+		/// <param name="handlerId"></param>
+		public void RedirectToHandler(Guid? handlerId)
+		{
+			var mobileNotificationMaint = PXGraph.CreateInstance<MobileNotificationMaint>();
+			mobileNotificationMaint.Notifications.Current = mobileNotificationMaint.Notifications.Search<MobileNotification.noteID>(handlerId);
+			PXRedirectHelper.TryRedirect(mobileNotificationMaint, PXRedirectHelper.WindowMode.New);
+		}
+	}
+}
